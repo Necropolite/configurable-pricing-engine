b9298e00852d3e5eea3362f41852664274a09196
Clinton Ramey <cramey254@gmail.com>
2026-08-23 11:23:07 -0400

    Add public package entry point

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+export { calculateQuote } from './engine';
+
+export type {
+  FixedRule,
+  PercentageRule,
+  PerUnitRule,
+  PricingCatalog,
+  PricingCondition,
+  PricingItem,
+  PricingOperator,
+  PricingRule,
+  PricingValue,
+  QuoteLine,
+  QuoteLineKind,
+  QuoteRequest,
+  QuoteResult,
+} from './types';
